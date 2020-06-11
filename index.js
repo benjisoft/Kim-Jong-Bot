b7eb8ecc1874c3119baa98b7b3725482d57c04d0
Benji Lewis <benji@benjisoft.org.uk>
2020-06-12 00:47:25 +0100

    Removed Google

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const discord = require('discord.js')
 const client = new discord.Client();
 var converter = require('number-to-words');
-const {Translate} = require('@google-cloud/translate').v2;
-const translate = new Translate();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -11,17 +9,7 @@ client.on('ready', () => {
 client.on('guildMemberAdd', member => {
     var size = member.guild.members.cache.size;
     var english = converter.toWords(size)
-    translateText(english, "ko")
-    member.setNickname("Kim Jon" + size);
+    member.setNickname("Kim Jon " + english);
 })
 
 client.login(process.env.discord_token);
-
-async function translateText(text, target) {
-  let [translations] = await translate.translate(text, target);
-  translations = Array.isArray(translations) ? translations : [translations];
-  console.log('Translations:');
-  translations.forEach((translation, i) => {
-    console.log(`${text[i]} => (${target}) ${translation}`);
-  });
-}
