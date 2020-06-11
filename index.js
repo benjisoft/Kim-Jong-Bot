f47f43094182160c6c71cfe3130dcaf426e291d4
Benji Lewis <benji@benjisoft.org.uk>
2020-06-11 23:50:03 +0100

    Discord Baseplate

---
+++ index.js
@@ -0,0 +1,8 @@
+const discord = require('discord.js')
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+
+client.login(process.env.discord_token);
